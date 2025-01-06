d05a3f3ac02d5f376ca1362b5e6a66e2aff0be6d
gregoirebouiller <gregoirebou95@gmail.com>
2025-01-06 12:06:31 -0300

    Plant selling website made with react. Part of IBM full stack certificate

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,12 +7,28 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const {name, image, cost} = action.payload;
+      const existingItem = state.items.find(item => item.name === name);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ name,image,cost, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
+      const item=action.payload;
+      const existingItem=state.items.find((i)=>i.name===item.name);
+      if(existingItem){
+        state.items=state.items.filter((i)=>i.name!==item.name);
+      }
     },
     updateQuantity: (state, action) => {
-
+      const { name, quantity } = action.payload;
+      const item = state.items.find((i) => i.name === name);
+      item.quantity = quantity;
+      if (item.quantity === 0) {
+        state.items = state.items.filter((i) => i.name !== name);
+      }
     
     },
   },
